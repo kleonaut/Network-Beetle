2afd804acfb3ca33c3cb21b5ab27b649994cfef0
Kleonaut <70820375+kleonaut@users.noreply.github.com>
2023-12-30 21:20:31 -0600

    renamed Application.java to App.java

--- src/com/github/kleonaut/network_beetle/Main.java
+++ src/com/github/kleonaut/network_beetle/Main.java
@@ -1,17 +1,14 @@
 package com.github.kleonaut.network_beetle;
 
-import java.awt.AWTException;
-import java.io.IOException;
-
 public class Main {
 
-    // TODO: read conditions from file
     // TODO: create a UI
+    // TODO: use Preferences to save changes
     // TODO: create a better, transparent icon
-    // NOTE: icon image may not be found after exporting to JAR
+    // TODO: when opening tray icon popup the app pauses everything...
 
     public static void main(String[] args) {
-        Application app = new Application();
+        App app = new App();
         app.start();
     }
 }
