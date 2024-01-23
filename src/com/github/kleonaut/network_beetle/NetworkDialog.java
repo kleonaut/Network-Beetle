c20e4c4b29594151f40ff19415b9a53201c85979
Kleonaut <70820375+kleonaut@users.noreply.github.com>
2024-01-22 22:50:16 -0600

    Added local network profiles to choose from

--- src/com/github/kleonaut/network_beetle/NetworkDialog.java
+++ src/com/github/kleonaut/network_beetle/NetworkDialog.java
@@ -1,32 +1,34 @@
 package com.github.kleonaut.network_beetle;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public class NetworkDialog
+public class NetworkDialog extends JDialog
 {
-    private final JDialog dialog;
     private final JTextArea nominatedNetwork;
     private final JList<String> networkList;
     private final ModeDialog returnWindow;
+    private static final Timer timer = new Timer(2000, null);
 
-    NetworkDialog(JDialog owner, ModeDialog returnWindow, NetProfile nowProfile)
+    NetworkDialog(JDialog owner, ModeDialog returnWindow, NetProfile assignedProfile)
     {
-        this.returnWindow = returnWindow;
-        dialog = new JDialog(owner, "Assign Network Profile", true);
-        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        super(owner, "Assign Network Profile", true);
+        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 
-        nominatedNetwork = new JTextArea(" "+nowProfile.name());
+        this.returnWindow = returnWindow;
+
+        nominatedNetwork = new JTextArea(" "+assignedProfile.name());
         nominatedNetwork.setEditable(false);
         nominatedNetwork.setFocusable(false);
 
         networkList = new JList<>(new DefaultListModel<>());
         networkList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         networkList.addListSelectionListener(e -> nominateSelection());
-        DefaultListModel<String>listModel = (DefaultListModel<String>)networkList.getModel();
-        listModel.addElement("Option 1");
-        listModel.addElement("Option 2");
-        listModel.addElement("Option 3");
+        networkList.setEnabled(false);
+        ((DefaultListModel<String>)networkList.getModel()).addElement(" Fetching...");
 
         JButton confirmButton = new JButton("Confirm");
         confirmButton.addActionListener(e -> confirmSelection());
@@ -41,16 +43,53 @@ public class NetworkDialog
         panel.add(confirmButton,
                   new Constraints(0, 2).anchor(6).get());
 
-        dialog.setContentPane(panel);
-        dialog.setSize(230, 250);
-        dialog.setLocationRelativeTo(owner);
-        dialog.setVisible(true);
+        setContentPane(panel);
+        setSize(230, 250);
+        setLocationRelativeTo(owner);
+
+        timer.addActionListener(e -> updateNetworkList());
+        addWindowListener(new WindowAdapter()
+        {
+            @Override
+            public void windowClosing(WindowEvent e)
+            {
+                timer.stop();
+                for (ActionListener listener : timer.getActionListeners())
+                    timer.removeActionListener(listener);
+            }
+        });
+        timer.setInitialDelay(400);
+        timer.start();
+
+        setVisible(true);
     }
 
-    public void nominateSelection() { nominatedNetwork.setText(" "+networkList.getSelectedValue()); }
+    public void updateNetworkList()
+    {
+        networkList.setEnabled(true);
+
+        String selectedNetwork = networkList.getSelectedValue();
+        int selectedIndex = -1;
+
+        DefaultListModel<String>listModel = (DefaultListModel<String>)networkList.getModel();
+        listModel.clear();
+        listModel.addElement(" " + NetProfile.STAY.name());
+        listModel.addElement(" " + NetProfile.DISCONNECT.name());
+
+        for (NetProfile profile : Networks.fetchAllProfiles())
+            listModel.addElement(" " + profile.name());
+
+        for (int i = 0; i < listModel.size(); i++)
+            if (listModel.get(i).equals(selectedNetwork))
+                selectedIndex = i;
+
+        networkList.setSelectedIndex(selectedIndex);
+    }
+
+    public void nominateSelection() { nominatedNetwork.setText(networkList.getSelectedValue()); }
     public void confirmSelection()
     {
         returnWindow.setNetwork(new NetProfile(nominatedNetwork.getText().trim()));
-        dialog.dispose();
+        dispose();
     }
 }
